34689be0253222b6099a2bf971b2fda748d08f9e
matt <mattcllhn@gmail.com>
2016-10-03 14:34:14 -0500

    omdb array filtering through hard coded rating

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,13 @@
 console.log('js sourced');
+var omdbArray = [];
+var flixArray = [];
+
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('testController',['$scope','$http',function($scope,$http){
   console.log('NG');
   $scope.search = function(searchIn){
-    console.log('serchIn',searchIn);
+    console.log('searchIn',searchIn);
     //compile url based on search fields filled out actor&director&ect.
     var omdbUrl = 'http://www.omdbapi.com/?t='+searchIn+'&r=json';
     var flixUrl = 'http://netflixroulette.net/api/api.php?actor='+searchIn+'%20';
@@ -13,24 +16,41 @@ myApp.controller('testController',['$scope','$http',function($scope,$http){
       url:flixUrl,
     }).then(function(flixData){
         compareMovies(flixData);
+        flixArray = flixData.data;
+        console.log('flixArray',flixArray);
     });//end http.then function
     function compareMovies(dataIn){
+      omdbArray = [];
       var results = dataIn.data;
-      console.log('data',results);
+      // console.log('data',results);
       $scope.dataBack=results;
 
       for (var i = 0; i < results.length; i++) {
-        console.log('results.show_title',results[i].show_title);
         omdbUrl = 'http://www.omdbapi.com/?t='+results[i].show_title+'&r=json';
         $http({
           method:'GET',
           url:omdbUrl
+
+          //----------------------------------maybe need to break this up because it's async -->getting errors in larger calls
         }).then(function(omdbData){
-          console.log('omdbData',omdbData);
+          // console.log('omdbData',omdbData);
+          if(Number(omdbData.data.imdbRating)<8){
+            console.log(omdbData.data.Title+' is not good enough');
+          }else{
+            console.log(omdbData.data.Title + ' is good enough');
+
+          omdbArray.push(omdbData.data);
+        }
           // $scope.omdbDataBack=omdbData;
+
         });//end http.then function
       }//for loop
+      console.log('omdbArray',omdbArray);
 
     }//compareMovies
   };//scope.searchIn
+  $scope.display = function(){
+    $scope.seeMovies = omdbArray;
+  };
+  $scope.display();
 }]);//myApp.testController
