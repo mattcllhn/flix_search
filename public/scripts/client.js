4ef85594280266b7a0df54fe011a1bd2387cf5f7
matt <mattcllhn@gmail.com>
2016-10-03 13:26:05 -0500

    hitting flix api and calling compare function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,23 @@ myApp.controller('testController',['$scope','$http',function($scope,$http){
   console.log('NG');
   $scope.search = function(searchIn){
     console.log('serchIn',searchIn);
-    var urlIn = 'http://www.omdbapi.com/?t='+searchIn+'&r=json';
+    //compile url based on search fields filled out actor&director&ect.
+    var omdbUrl = 'http://www.omdbapi.com/?t='+searchIn+'&r=json';
+    var flixUrl = 'http://netflixroulette.net/api/api.php?actor='+searchIn+'%20';
     $http({
       method:'GET',
-      url:urlIn,
-    }).then(function(data){
-      $scope.dataBack = data;
-
+      url:flixUrl,
+    }).then(function(flixData){
+        compareMovies(flixData);
     });//end http.then function
+    function compareMovies(dataIn){
+      console.log('data',dataIn.data);
+      $scope.dataBack=dataIn.data;
 
-  };
+      for (var i = 0; i < dataIn.length; i++) {
+
+      }//for loop
+
+    }//compareMovies
+  };//scope.searchIn
 }]);//myApp.testController
